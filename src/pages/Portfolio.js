f359ce5bc9c6501c57e848216c37186eb2616889
mohanad.saqr <mohanad.saqr@alum.utoronto.ca>
2024-03-31 23:47:01 -0400

    updated links

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -12,29 +12,29 @@ const Portfolio = () => {
     // Add more projects as needed
     { title: 'Daily Reminder Guide', 
     imageUrl: require('../assets/dr.png'), 
-    deployedUrl: 'https://github.com/mohanad-saqr/Daily-Reminder-Guide', 
-    repoUrl: 'https://project2-heroku-deployment-b854ca21b876.herokuapp.com/' 
+    deployedUrl: 'https://project2-heroku-deployment-b854ca21b876.herokuapp.com/', 
+    repoUrl: 'https://github.com/mohanad-saqr/Daily-Reminder-Guide' 
   },
   { title: 'Text Editor', 
   imageUrl: require('../assets/te.png'), 
-  deployedUrl: 'https://github.com/mohanad-saqr/Text-Editor', 
-  repoUrl: 'https://github.com/mohanad-saqr/Code-Quiz' 
+  deployedUrl: 'https://text-editor-ms-1e275d9bc320.herokuapp.com/', 
+  repoUrl: 'https://github.com/mohanad-saqr/Text-Editor' 
 },
 { title: 'Work Day Scheduler', 
 imageUrl: require('../assets/wds.png'), 
-deployedUrl: 'https://github.com/mohanad-saqr/Work-Day-Scheduler', 
-repoUrl: 'https://mohanad-saqr.github.io/Work-Day-Scheduler/' 
+deployedUrl: 'https://mohanad-saqr.github.io/Work-Day-Scheduler/', 
+repoUrl: 'https://github.com/mohanad-saqr/Work-Day-Scheduler' 
 },
-// Add more projects as needed
+
 { title: 'Weather Dashboard', 
 imageUrl: require('../assets/wd.png'), 
-deployedUrl: 'https://github.com/mohanad-saqr/Weather-Dashboard', 
-repoUrl: 'https://mohanad-saqr.github.io/Weather-Dashboard/' 
+deployedUrl: 'https://mohanad-saqr.github.io/Weather-Dashboard/', 
+repoUrl: 'https://github.com/mohanad-saqr/Weather-Dashboard' 
 },
 { title: 'Regex Tutorial', 
 imageUrl: require('../assets/re.png'), 
-deployedUrl: 'https://github.com/mohanad-saqr/Regex-Tutorial', 
-repoUrl: 'https://gist.github.com/mohanad-saqr/4c8cb61ecfab9c1ebeebf043070e1743' 
+deployedUrl: 'https://gist.github.com/mohanad-saqr/4c8cb61ecfab9c1ebeebf043070e1743', 
+repoUrl: 'https://github.com/mohanad-saqr/Regex-Tutorial' 
 },
   ];
 
